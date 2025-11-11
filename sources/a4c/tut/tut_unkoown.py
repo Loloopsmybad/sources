1be67ca1f7b270bd91134e9c144a30685e5cc96e
JKS-LAPTOP\nigger <kanishk.luckymidnight@gmail.com>
2025-11-11 10:45:19 +0530

    no message

---
+++ sources/a4c/tut/tut_unkoown.py
@@ -0,0 +1,28 @@
+def palindrme(arr,cut):
+    if len(arr)<=1:
+        return a
+        
+    mid=len(arr)//2    
+    left=palindrme(arr[:mid])
+    right=palindrme(arr[mid:])
+
+    
+    i=0
+    j=len(right)-1
+    m=[]
+    while j >= 0 :
+        if left[i]==right[j]:
+            i+=1
+            j-=1
+            cut+=1
+        else:
+            i+=1
+            j-=1
+    m=left+right
+
+
+    return m,cut
+    
+stri = input("nig")
+cut=0
+palindrme(stri)
