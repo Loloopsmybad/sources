0f2c4306a5335bfafdcf6c4556b559c590bd56e3
nigger <mygundonotwork@outlook.com>
2025-11-24 12:21:22 +0530

    no message

--- sources/ip project/main.py
+++ sources/ip project/main.py
@@ -13,22 +13,59 @@ def load_stations(filename):
     except FileNotFoundError:
         return f"Error: File '{filename}' not found."
 
-def calculate_next_train(current_station, stations, current_time):
+def calculate_next_train(current_station, stations, current_time, margenta,blueline_4,blueline_5):
         try:
             FIRST_TRAIN_START = "05:00"
             TRAVEL_TIME_PER_STATION = 2  
             TRAIN_INTERVAL = 8 
             station_index=0
-            for i in range(len(stations)):
-               
-                    if stations[i][1] ==current_station:
-                        
-                        break
-                    station_index+=1
-            destination_station_index=0
-            
-            
-            #print(station_index)
+            destination_index=0
+            # if current_station in margenta:
+            #     station_index=margenta.index(current_station)
+            # elif current_station in blueline_1:
+            #     station_index=blueline_1.index(current_station)
+            # elif current_station in blueline_3:
+            #     station_index=blueline_3.index(current_station)
+
+            # for i in range(len(stations)):
+            #             if stations[i][1] ==current_station:   
+            #                 break
+            #             station_index+=1
+            print(destination_station in margenta)
+            if current_station in margenta and destination_station in margenta :
+                print("hi")
+                if margenta.index(current_station)<margenta.index(destination_station):
+                    station_index=margenta.index(current_station)
+                    destination_index=margenta.index(destination_index)-station_index
+                elif margenta.index(current_station)>margenta.index(destination_station):
+                    margenta=margenta[::-1]
+                    station_index=margenta.index(current_station)
+                    destination_index=abs(margenta.index(destination_station)-station_index)
+            elif current_station in blueline_4 and destination_station in blueline_4 :
+                print("hi")
+                if blueline_4.index(current_station)<blueline_4.index(destination_station):
+                    station_index=blueline_4.index(current_station)
+                    destination_index=blueline_4.index(destination_station)-station_index
+                elif blueline_4.index(current_station)>blueline_4.index(destination_station):
+                    blueline_4=blueline_4[::-1]
+                    station_index=blueline_4.index(current_station)
+                    destination_index=abs(blueline_4.index(destination_station)-station_index)
+            elif current_station in blueline_5 and destination_station in blueline_5 :
+                print("hi")
+                if blueline_5.index(current_station)<blueline_5.index(destination_station):
+                    station_index=blueline_5.index(current_station)
+                    destination_index=blueline_5.index(destination_station)-station_index
+                elif blueline_5.index(current_station)>blueline_5.index(destination_station):
+                    blueline_5=blueline_5[::-1]
+                    station_index=blueline_5.index(current_station)
+                    destination_index=abs(blueline_5.index(destination_station)-station_index)
+
+            # elif current_station in margenta and destination_station in blueline_3:
+            #         station_index=margenta.index(current_station)
+            #         if station.index()
+            #         destination_index=blueline_3.index(destination_index)
+            print(station_index)
+            print(destination_index)
             
 
             a=current_time
@@ -50,17 +87,11 @@ def calculate_next_train(current_station, stations, current_time):
 
                     arrival_hour = int(train_arrival_time) // 60
                     arrival_minute = train_arrival_time-(arrival_hour*60)
-                    #print("minutes",arrival_minute)
-                
-
                     wait_time = train_arrival_time - current_minutes
-                    #wait_hour = int(wait_time // 60)
-                    #wait_minute = int(wait_time % 60)
-                    #print(f"{wait_minute}")
-                    
                     
                     return {   
-                        "station": current_station,
+                        "current station": current_station,
+                        "destination station": destination_station,
                         "arrival time": f"{arrival_hour}:{arrival_minute}",
                         "wait minutes": wait_time,
                         "current time": a
@@ -69,53 +100,10 @@ def calculate_next_train(current_station, stations, current_time):
                 train_number += 1
         except:
             return "error"
-def calculate_next_train(current_station, stations, current_time):
-        try:
-            
-            station_index=0
-            for i in range(len(stations)):
-               
-                    if stations[i][1] ==current_station:
-                        
-                        break
-                    station_index+=1
-            
-            #print(station_index)
-            
 
-            a=current_time
-            current_time=current_time.split(":")
-            current_minutes = int(current_time[0]) * 60 + int(current_time[1])
-            #print(f"{current_minutes}min")
-            FIRST_TRAIN_START=FIRST_TRAIN_START.split(":")
-            FIRST_TRAIN_START = int(FIRST_TRAIN_START[0]) * 60 + int(FIRST_TRAIN_START[1])
 
-            per_station_delay = station_index * TRAVEL_TIME_PER_STATION 
-            # print("hi1",per_station_delay)
-            first_train_arrival_to_station = FIRST_TRAIN_START + per_station_delay
-            # print("hi",first_train_arrival_to_station)
-            train_number = 0
-            while True:
-                train_arrival_time = first_train_arrival_to_station + (train_number * TRAIN_INTERVAL)
-           
-                if train_arrival_time >= current_minutes:
 
-                    arrival_hour = int(train_arrival_time) // 60
-                    arrival_minute = train_arrival_time-(arrival_hour*60)
-           
-                
 
-                    wait_time = train_arrival_time - current_minutes
-                   
-                    
-                    
-                    return {   
-                        
-                        }
-                
-                train_number += 1
-        except:
-            return "error"
 
 print("=" * 50)
 print("TRAIN ARRIVAL CALCULATOR")
@@ -123,40 +111,33 @@ print("=" * 50)
 
 filename = input("Enter the station list filename:").strip()
 
-stations,cmplt_info = load_stations(filename)
+stations_only,cmplt_info = load_stations(filename)
 
-margenta=stations[:25]
-m_e=25
-blueline_1=stations[42:50]
-b1_s=42
-b1_e=50
-blueline_2=stations[26:42]
-blueline_3=stations[50:100]
-b3_s=50
-b3_e=100
+margenta=stations_only[:25]#margenta
+blueline_1=stations_only[42:50]#vaishali line
+blueline_2=stations_only[26:42]#ele to akshardham
+blueline_3=stations_only[67:100]#inderpreastha to dwarka
+blueline_4=stations_only[50:100]#main blueline from ele
+blueline_5=blueline_1+blueline_3#main blueline from vaishali
 
-for i, station in enumerate(stations, 1):
+
+for i, station in enumerate(stations_only, 1):
     if station in margenta and station in blueline_1 :
-        print(f"{i}.    station on both blue and martgenta lines: {station}")
-    elif station in margenta and station in blueline_3 :
-        print(f"{i}.    station on both blue and martgenta lines: {station}")
+        print(f"{i}.    station on both blue and margenta lines: {station}")
+    elif station in margenta and station in blueline_4 :
+        print(f"{i}.    station on both blue and margenta lines: {station}")
     elif station in margenta:
         print(f"{i}.    margenta line station: {station}")
-    elif station in blueline_1 or blueline_3:
+    elif station in blueline_1 or blueline_4:
         print(f"{i}.    blue line station: {station}")
      
 print("\n" + "=" * 50)
-current_line=input("Enter your current station line: ").strip().upper()
+
 current_station = input("Enter your current station name: ").strip()
-destination_line=input("Enter your destination station line: ").strip()
-destination_station=input("Enter your destination station name: ").strip().upper()
-interchange_station=input("Enter the station name at which you want to change line ('NA' if not change):")
+destination_station=input("Enter your destination station name: ").strip()
 current_time=input("Enter current time (kindly enter in 24 hrs format) :").strip()
-if interchange_station=="NA":
-    interchange_station=current_line
 
-
-result = calculate_next_train(current_station, cmplt_info,current_time)
+result = calculate_next_train(current_station, cmplt_info,current_time,margenta,blueline_4,blueline_5)
 
 print("\n" + "=" * 50)
 print("TRAVEL DETAILS")
@@ -165,10 +146,10 @@ print("=" * 50)
 if "error" in result:
     print("Error")
 else:
-    print(f"Current Station line: {current_line}")
-    print(f"Current Station: {result['station']}")
-    print(f"Destination Station line: {destination_line}")
-    print(f"Destination Station: {result['station']}")
+    print(f"Current Station line: {cmplt_info[stations_only.index(current_station)][0]}")
+    print(f"Current Station: {result['current station']}")
+    print(f"Destination Station line: {cmplt_info[stations_only.index(destination_station)][0]}")
+    print(f"Destination Station: {result['destination station']}")
     print(f"Current Time: {result['current time']}")
     print(f"Train Arrival Time: {result['arrival time']}")
     print(f"Waiting Time: {result['wait minutes']} minutes")
