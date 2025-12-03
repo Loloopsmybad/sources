e956c05260b3f57ecf9eda028c6453647bef29af
nigger <mygundonotwork@outlook.com>
2025-12-03 23:58:48 +0530

    no message

---
+++ sources/sources/sumOfactorial.py
@@ -0,0 +1,12 @@
+n = int (input ("enter a number -->")) # type: ignore
+k=1
+d=0
+for i in range (1,n+1):
+        for j in range (1,i+1):
+            k = k*j 
+        d = d+k
+        k=1
+print("sum of factorial in series is-->",d)
+
+            
+
