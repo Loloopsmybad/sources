d48f6ae76be0e20ef87c39192b24dd5ca392813c
nigger <mygundonotwork@outlook.com>
2025-12-11 23:22:54 +0530

    no message

---
+++ sources/a4c/c++/dsa101.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a = -1;
+    int spacing =6;
+    std::cout<<"hello world"<<endl;
+    for (int i=0; i<8;i++){
+        for (int k=0; k<=spacing;k++){
+            cout<<" ";
+        }
+        for (int j=0;j<=i;j++){
+        cout<<"@";
+    a++;
+}
+       
+        for (int p=0; p<a; p++){
+        cout<<"@";}
+        cout<<endl;
+        spacing--;
+        a=-1;
+        }
+
+spacing=0;
+a=6;
+        for (int i=0; i<8;i++){
+            for (int k=0; k<=spacing;k++){
+            cout<<" ";
+            }
+            for (int j=6;j>=i;j--){
+            cout<<"@";
+            a--;
+            }
+       
+            for (int p=4; p>=a; p--){
+            cout<<"@";}
+
+            cout<<endl;
+            spacing++;
+            a=6;
+        }
+    std::getchar();
+}
