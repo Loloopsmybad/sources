9ae378722eba54de58201ece104e318165a8f557
nigger <mygundonotwork@outlook.com>
2025-12-13 15:18:59 +0530

    halo

--- sources/a4c/c++/test.cpp
+++ sources/a4c/c++/test.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string arra = "nigger";
+string arra ;
+int ara[3]={1,2,34};
+string lst[10];
+ 
 int main(){
-
-    cout<<"hi"<<arra;
-    std::getchar();
-
+    getline(cin,arra);
+    cout<<"hi "<<arra<<" "<< arra.length()<<endl;
+    
+    for (int i=0;i<=arra.length();i++){
+        cout<<arra[arra.length()-i]<<endl;
+    }
+for (int i : ara){
+    cout<< i;
+}
+cout<<endl;
+int i=0;
+while (i<5){
+    string a;
+    cin>>a;
+    lst[i]=a;
+    i++;
+}
+for (string a: lst){
+cout<<a<<":::";}
+cout<<endl;
+system("pause");
+return 0;
+	
 }
