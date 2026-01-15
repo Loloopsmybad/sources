320b5eb989feae7a4e3422ebf6dec0079e00e88b
nigger <mygundonotwork@outlook.com>
2026-01-15 23:53:35 +0530

    no message

---
+++ sources/a4c/c/sorting.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+int arr[]={2,4,6,33,7,8,12};
+int a = sizeof(arr)/sizeof(arr[0]);
+for (int i =0; i<a;i++){
+
+
+}
+
+
+system("pause");
+return 0;
+}
