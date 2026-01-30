47bc86f43a3131771deebc58ec26557cb3e9480e
nigger <mygundonotwork@outlook.com>
2026-01-30 14:51:15 +0530

    no message

---
+++ sources/a4c/c/testing/selection sort.c
@@ -0,0 +1,56 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void selectionsort(int *arr, int *size){
+    int i=0,j=0;
+    int temp=arr[0];
+    int temp_val;
+    int temp_index;
+    printf("\n");
+    printf("______%d______",(*size));
+    while (i<(*size)){
+        
+        if (arr[j]>=temp){//just change the sign for reversing 
+            temp=arr[j];
+            temp_index=j;
+            j++;
+        }else{
+            j++;
+        }
+        if (j==(*size)){
+            temp_val=arr[i];
+            arr[i]=temp;
+            arr[temp_index]=temp_val;
+            i++;
+            j=i;    
+            temp=arr[i];
+        }
+    }
+}
+
+
+
+int main(){
+    printf("no of elements ?");
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("number ? ");
+        scanf("%d",&arr[i]);
+    }
+    printf("\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    int size =sizeof(arr)/sizeof(arr[0]);
+
+    selectionsort(arr,&size);
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+system("pause");
+return 0;
+
+}
