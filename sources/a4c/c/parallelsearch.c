10f9bb9236b8f1586969b679648b0f49e55f697d
nigger <mygundonotwork@outlook.com>
2026-02-11 23:11:54 +0530

    no message

--- sources/a4c/c/parallelsearch.c
+++ sources/a4c/c/parallelsearch.c
@@ -1,51 +1,62 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void parallelsearch(int *a1,int *a2,int x,int s1,int s2){
-    for (int i=0;i<s1;i++){
-        for (int j=0;j<s2;j++){
-            if( a1[i]+a2[j]==x){
-                printf("%d + %d = %d",a1[i],a2[j], x);
-            }
+
+
+int binarysearch(int*a2,int n,int req){
+    int left = 0;
+    int right = n - 1;
+    while(left <= right) {
+    int mid = left + (right - left) / 2;
+    if (a2[mid] == req){
+        return mid;
+        }
+    else if (a2[mid] < req){
+        left = mid + 1;
+        }
+    else{
+        right = mid - 1;
+        }
+    }
+    return 0;
+}
+
+
+void search(int *a1, int * a2, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        int required = x - a1[i];
+        int right_ptr = binarysearch(a2, n, required);
+        if (right_ptr != 0){
+            printf(" pair is { %d + %d } = %d \n", a1[i],a2[right_ptr],x);
+            return;
         }
     }
 }
 
-int main(){
-    int n1;
-    int n2;
+int main() {
+    int n;
     int x;
-    printf("number of elements in array 1?");
-    scanf("%d",&n1);
-    int a1[n1];
-    for (int j =0;j<n1;j++){
-        printf("number ?");
-        scanf("%d",&a1[j]);
+    
+    printf("enter the size of arrays: ");
+    scanf("%d", &n);
+    
+    int a1[n]; 
+    int a2[n];
+    
+    printf("enter %d elements of array A: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a1[i]);
     }
-    printf("number of elements in array 2?");
-    scanf("%d",&n2);
-    int a2[n2];
-    for (int j =0;j<n2;j++){
-        printf("number ?");
-        scanf("%d",&a2[j]);
+    
+    printf("enter %d elements of array B: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a2[i]);
     }
-    printf("\n =================arrayyyy==================\n");
-    for (int j =0;j<n1;j++){
-        printf("%d ", a1[j]);
-    }
-    printf("\n");  
-    for (int j =0;j<n2;j++){
-        printf("%d ", a2[j]);
-    }
-    int s1=sizeof(a1)/sizeof(a1[0]);
-    int s2=sizeof(a2)/sizeof(a2[0]);
-    printf("\n%d\n",s1);
-    printf("%d\n",s2);
-    printf("\n");  
-    printf("number u want to find pair for ?");
-    scanf("%d",&x);
-    printf("\n");  
-    parallelsearch(a1,a2,x,s1,s2);
+    
+    printf("the target sum x: ");
+    scanf("%d", &x);
+    
+    search(a1,a2,n,x);
     system("pause");
     return 0;
 }
